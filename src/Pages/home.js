2101271d8ff4f74facd50a92028c7c8b65b64cc4
Kacper <kacper.tylec1999@gmail.com>
2022-08-20 20:49:52 +0200

    working on home page

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./home.css"
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+  <div className="home">
+    <div className="image--container">
+      <button className="image--button">Create your workout plan</button>
+    </div>
+
+
+
+
+  </div>
+  )
 };
 
 export default Home;
