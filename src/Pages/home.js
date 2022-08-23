76206b889989f2787bfbe88f0ee763297d23bae5
Kacper <kacper.tylec1999@gmail.com>
2022-08-24 00:25:20 +0300

    Adding cards to Home and working on workoutForm

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import "./home.css"
 import { Link } from "react-router-dom"
+import * as FaIcons from "react-icons/fa";
+import * as BiIcons from "react-icons/bi";
+
 
 const homeButtonLinkStyle= {
   textDecoration: "none",
@@ -8,9 +11,53 @@ const homeButtonLinkStyle= {
 }
 
 const Home = () => {
+  const [cardsTextIsVisible, toggleCardTextVisible] = useState({
+    firstCard: false,
+    secondCard: false,
+    thirdCard: false
+  })
+  const showFirstText = () => {
+    toggleCardTextVisible(prevToggles => {
+      return {
+        ...prevToggles,
+        firstCard: !prevToggles.firstCard,
+      }
+    })
+  }
+  const showSecondText = () => {
+    toggleCardTextVisible(prevToggles => {
+      return {
+        ...prevToggles,
+        secondCard: !prevToggles.secondCard,
+      }
+    })
+  }
+  const showThirdText = () => {
+    toggleCardTextVisible(prevToggles => {
+      return {
+        ...prevToggles,
+        thirdCard: !prevToggles.thirdCard,
+      }
+    })
+  }
+
   return (
   <div className="home">
     <div className="image--container">
+      <div className="cards--container">
+        <div className="card" onClick={showFirstText}>
+          <BiIcons.BiBody />
+          {cardsTextIsVisible.firstCard && <span >asda sdsad sadad asda sd</span>}
+        </div>
+        <div className="card" onClick={showSecondText}>
+          <FaIcons.FaDumbbell />
+          {cardsTextIsVisible.secondCard && <span >asda sdsad sadad asda sd</span>}
+        </div>
+        <div className="card" onClick={showThirdText}>
+          <FaIcons.FaBookOpen />
+          {cardsTextIsVisible.thirdCard && <span >asda sdsad sadad asda sd</span>}
+        </div>
+      </div>
       <button className="image--button"><Link to={"Workouts"} style={homeButtonLinkStyle}> Create your workout plan</Link></button>
     </div>
 
--- src/workoutComponents/WorkoutForm.js
+++ src/workoutComponents/WorkoutForm.js
@@ -3,7 +3,50 @@ import React from 'react'
 const WorkoutForm = (props) => {
   return (
     <div className='workoutform'>
+      <div className='workoutform--row'>
+        <div>
+          <label>asd</label>
+          <input></input>
+        </div>
+        <div>
+          <label>asd</label>
+          <input></input>
+        </div>
+        <div>
+          <label>asd</label>
+          <input></input>
+        </div>
+      </div>
+      <div className='workoutform--row'>
+        <div>
+          <label>asd</label>
+          <input></input>
+        </div>
+        <div>
+          <label>asd</label>
+          <input></input>
+        </div>
+        <div>
+          <label>asd</label>
+          <select>
+            <option></option>
+            <option></option>
+            <option></option>
+          </select>
+        </div> 
+      </div>
+      <div className='workoutform--row'>
+        <div>
+          <label>asd</label>
+         <select>
+            <option></option>
+           <option></option>
+          </select>
+        </div>
+        <button>delete</button>
         <button onClick={props.hideNewWorkout}>Save</button>
+      </div>
+        
 
     </div>
   )
